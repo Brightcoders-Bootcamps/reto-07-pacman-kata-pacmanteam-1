9233f56b4e7ddbf5c1783450c73c3120ff26a3f7
Andres Pacheco <andrespach4@gmail.com>
2020-08-04 01:11:05 -0500

    Validation of the board values was performed

--- app/board.rb
+++ app/board.rb
@@ -27,8 +27,19 @@ class Board
   def board_iteration(pacman)
     loop do
       board_element = board[pacman.coordinate_row, pacman.coordinate_column - 1]
-    
+      pacman_steps_validations(board_element, pacman)
       break if pacman.dead?
+    end
+  end
+ 
+  def pacman_steps_validations(board_element, pacman)
+    if board_element.class == Ghost
+      pacman.dead = true
+    elsif board_element == 'o'
+      pacman.update_score
+      board[coordinate, coordinate] = pacman
+      board[pacman.coordinate_row, pacman.coordinate_column - 1] = pacman
+    elsif board_element == '|'
     end
   end
 
