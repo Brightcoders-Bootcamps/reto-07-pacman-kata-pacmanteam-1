0a0b4c596e539b5774020ee298098bf1ae9078fb
Andres Pacheco <andrespach4@gmail.com>
2020-08-04 23:59:46 -0500

    Add validations to pacman positions

--- app/board.rb
+++ app/board.rb
@@ -26,20 +26,31 @@ class Board
 
   def board_iteration(pacman)
     loop do
-      board_element = board[pacman.coordinate_row, pacman.coordinate_column - 1]
+      board_element = board[pacman.coordinate_row - 1, pacman.coordinate_column] 
       pacman_steps_validations(board_element, pacman)
       break if pacman.dead?
     end
   end
- 
+
   def pacman_steps_validations(board_element, pacman)
     if board_element.class == Ghost
       pacman.dead = true
     elsif board_element == 'o'
       pacman.update_score
-      board[coordinate, coordinate] = pacman
-      board[pacman.coordinate_row, pacman.coordinate_column - 1] = pacman
+      board[pacman.coordinate_row - 1, pacman.coordinate_column] = pacman
+      pacman.coordinate_row = pacman.coordinate_row - 1
     elsif board_element == '|'
+      board_element = board[pacman.coordinate_row , pacman.coordinate_column - 1]
+      if board_element == 'o'
+        pacman.update_score
+        board[pacman.coordinate_row - 1, pacman.coordinate_column] = pacman
+        pacman.coordinate_row = pacman.coordinate_row - 1
+      elsif board_element.class == Ghost
+        pacman.dead = true 
+      elsif board_element == '-'
+        board_element = board[pacman.coordinate_row , pacman.coordinate_column + 1]
+      end
+    elsif board_element == '-'
     end
   end
 
